edd988308a31774a699d9717b0342bafbb76a0f0
EchoingTime <bookofechoes@gmail.com>
2024-10-21 12:53:09 -0400

    __init__.py Update

---
+++ dementia_project/__init__.py
@@ -0,0 +1,35 @@
+"""
+URL: https://github.com/EchoingTime/dementia-project.git
+
+Description:
+
+Machine learning in medicine: Classification and prediction of dementia by support vector machines (SVM) is a study
+exploring the use of support vector machines for predicting dementia from a longitudinal group of 150 subjects aged 60
+to 96, including 373 subjects with MRI data. This study focuses on using machine learning algorithms to ease the
+medical industry with the collection of medical data, which in this case uses machine learning algorithms to predict
+dementia and check on possible dementia progress.
+
+Objective:
+
+To apply data mining techniques to the dementia data set and draw connections, e.g., age related to dementia,
+by extracting useful and relevant information.
+
+Preliminary Research Questions:
+
+1) Conduct data cleaning of the Dementia dataset by removing duplicates, handling missing values, and correcting
+inconsistencies.
+
+2) Understanding risk factors with data clustering by revealing risk factors among patients in similar clusters and
+helping to identify the potential progress of dementia.
+
+3) Using association rules for patient segmentation can encounter existing relationships and patterns among patients with
+related conditions.
+
+4) Construct a decision tree to categorize data by predicting the possibility of a patient developing dementia based on
+clinical and demographic data.
+
+Programmed By: Dante Anzalone, Dustin McDonnell, and Leidy Rojas Huisa
+Course: CSCI 4105: Knowledge Discovery and Data Mining
+Professor: Professor Wei
+Date: 2024-10-21
+"""
