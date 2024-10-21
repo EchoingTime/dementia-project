215ec90fd16ee37474a4c9f0d2c29da2a33bc27c
EchoingTime <bookofechoes@gmail.com>
2024-10-21 13:50:48 -0400

    preprocess.py Added load_data (file_name) function

--- dementia_project/preprocess.py
+++ dementia_project/preprocess.py
@@ -11,3 +11,15 @@ Course: CSCI 4105: Knowledge Discovery and Data Mining
 Professor: Professor Wei
 Date: 2024-10-21
 """
+# Importing libraries
+import pandas as pd
+
+# Importing Dataset via utilizing read_excel
+def load_data (file_name):
+    """
+    Accepts a file and loads the data into a pandas dataframe.
+    :param file_name: Name of the file
+    :return: Pandas dataframe
+    """
+    excel_dataframe = pd.read_excel (file_name)
+    return excel_dataframe
