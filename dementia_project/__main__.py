73a10fbcb7e1dad30eea3042732a74542f98b693
EchoingTime <bookofechoes@gmail.com>
2024-10-21 12:50:12 -0400

    Initial commit

---
+++ dementia_project/__main__.py
@@ -0,0 +1,10 @@
+"""
+Used to run project's programs
+
+Programmed By: Dante Anzalone, Dustin McDonnell, and Leidy Rojas Huisa
+Course: CSCI 4105: Knowledge Discovery and Data Mining
+Professor: Professor Wei
+Date: 2024-10-21
+"""
+if __name__ == '__main__':
+    pass
---
+++ dementia_project/preprocess.py
@@ -0,0 +1,13 @@
+"""
+Phase One: The preprocessing of the datasets
+
+Handling, for data cleaning, missing data, duplicated data, outliers, and zeros.
+
+Will use processing techniques for scaling normalization (standardization and normalization)
+and sampling (random sampling)
+
+Programmed By: Dante Anzalone, Dustin McDonnell, and Leidy Rojas Huisa
+Course: CSCI 4105: Knowledge Discovery and Data Mining
+Professor: Professor Wei
+Date: 2024-10-21
+"""
