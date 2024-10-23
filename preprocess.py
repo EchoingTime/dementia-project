3510df228fcad36cf9fc2d84c56147ea46bc062e
EchoingTime <bookofechoes@gmail.com>
2024-10-22 23:43:14 -0400

    Edit to preprocess.py class description

--- preprocess.py
+++ preprocess.py
@@ -1,10 +1,9 @@
 """
 Phase One: The preprocessing of the datasets
 
-Data Cleaning: Handling missing data, duplicated data, outliers, and zeros.
+Data Cleaning: Handling missing data, duplicated data, and outliers.
 
-Will use processing techniques for scaling normalization (standardization and normalization)
-and sampling (random sampling)
+Will use processing techniques for scaling normalization and sampling (random sampling)
 
 Programmed By: Dante Anzalone, Dustin McDonnell, and Leidy Rojas Huisa
 Course: CSCI 4105: Knowledge Discovery and Data Mining
